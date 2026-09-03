0bc18eda6f1436a67d5af3a9691b352f45b51e80
Shewale41 <shewalegopal4119@gmail.com>
2026-09-03 23:17:03 +0530

    feat:dynamic drop route added

--- server/src/routes/drop.routes.ts
+++ server/src/routes/drop.routes.ts
@@ -25,4 +25,20 @@ router.get("/", (_req, res) => {
   res.json(drops);
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  const result = drops.find((drop) => drop.id === id);
+
+  if (result) {
+    res.json(result);
+    return;
+  }
+
+  console.log("Drop doesn't exist");
+  res.status(404).json({
+    message: "Resource doesn't exist",
+  });
+});
+
 export default router;
