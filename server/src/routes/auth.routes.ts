868e933ff991dcc77fc9a8ad9a239ead2ca82dde
Shewale41 <shewalegopal4119@gmail.com>
2026-09-04 16:02:28 +0530

    feat:user login and register completed

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -6,24 +6,91 @@ import { randomUUID } from "node:crypto";
 const router = Router();
 
 router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  const id = randomUUID();
+      if (!name || !email || !password) {
+          return res.status(400).json({
+              message: "Name, email and password are required",
+          });
+      }
 
-  const hashPassword = await bcrypt.hash(password, 10);
+    const id = randomUUID();
 
-  const result = await pool.query(
-    `INSERT INTO users (id, name, email, password_hash)
-     VALUES ($1, $2, $3, $4)
-     RETURNING *`,
-    [id, name, email, hashPassword]
-  );
+    const hashPassword = await bcrypt.hash(password, 10);
 
-  return res.status(201).json({
-    id: result.rows[0].id,
-    name: result.rows[0].name,
-    email: result.rows[0].email,
-  });
+    const result = await pool.query(
+      `INSERT INTO users (id, name, email, password_hash)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [id, name, email, hashPassword]
+    );
+
+    return res.status(201).json({
+      id: result.rows[0].id,
+      name: result.rows[0].name,
+      email: result.rows[0].email,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+
+    const result = await pool.query(
+      `SELECT *
+       FROM users
+       WHERE email = $1`,
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    const user = result.rows[0];
+
+    const passwordMatch = await bcrypt.compare(
+      password,
+      user.password_hash
+    );
+
+    if (!passwordMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 });
 
 
