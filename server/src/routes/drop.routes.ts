c8a659abdc04de6efa94b982570f20b251bb1dda
Shewale41 <shewalegopal4119@gmail.com>
2026-09-04 13:39:56 +0530

    feat:implement drops CRUD API

--- server/src/routes/drop.routes.ts
+++ server/src/routes/drop.routes.ts
@@ -60,4 +60,47 @@ router.post("/", async (req, res) => {
   return res.status(201).json(addedRow.rows[0]);
 });
 
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const { name, availableInventory } = req.body;
+
+  const updatedRow = await pool.query(
+    `UPDATE drops
+     SET name = $1,
+         available_inventory = $2
+     WHERE id = $3
+     RETURNING *`,
+    [name, availableInventory, id]
+  );
+
+  if (updatedRow.rows.length === 0) {
+    return res.status(404).json({
+      message: "Drop not found",
+    });
+  }
+
+  res.status(200).json(updatedRow.rows[0]);
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const deletedRow = await pool.query(
+    "DELETE FROM drops WHERE id = $1 RETURNING *",
+    [id]
+  );
+
+  if (deletedRow.rows.length === 0) {
+    return res.status(404).json({
+      message: "Drop not found",
+    });
+  }
+
+  res.status(200).json({
+    message: "Drop deleted successfully",
+    drop: deletedRow.rows[0],
+  });
+});
+
 export default router;
